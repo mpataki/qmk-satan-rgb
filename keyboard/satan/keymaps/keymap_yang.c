72a4e1d16efb9a83fd95766f9299c8f68653e182
TerryMathews <terry@terrymathews.net>
2016-03-26 16:22:09 -0400

    Keymap swap
    Switched position of the right hand mod keys to have swapping into and
    out of _MV handled on the same key.

--- keyboard/satan/keymaps/keymap_yang.c
+++ keyboard/satan/keymaps/keymap_yang.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
    * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space            |Alt |Gui |CTRL| _MV |
+   * |Ctrl|Gui |Alt |      Space            |_MV |ALT |GUI |CTRL |
    * `-----------------------------------------------------------'
    */
 [_BW] = KEYMAP(
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,  KC_RBRC,  KC_BSLS, \
   F(11),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,            KC_ENT,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,            KC_RSFT, \
-  KC_LCTL, KC_LGUI,KC_LALT,          KC_SPC,                                       KC_RALT,KC_RGUI,  KC_RCTL,  F(9)),
+  KC_LCTL, KC_LGUI,KC_LALT,          KC_SPC,                                        F(9),  KC_RALT,  KC_RGUI,  KC_RCTL),
   /* Keymap _BM: (Base Mac) Mac Default Layer
    * ,-----------------------------------------------------------.
    * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |        | F1|F2 | F3|F4 | F5| F6| F7| F8|   |   |Up        |
    * |-----------------------------------------------------------|
-   * |    |    |    |         FN9            |    |Left|Down|Rig |
+   * |    |    |    |         FN9            |_BW |Left|Down|Rig |
    * `-----------------------------------------------------------'
    */
 [_MV] = KEYMAP(
