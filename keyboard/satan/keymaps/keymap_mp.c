08710733cfc2a69fb1945017495e085abc1022c0
Mat Pataki <matpataki@gmail.com>
2018-03-18 12:10:40 -0400

    simplify some more, and map control to my liking

--- keyboard/satan/keymaps/keymap_mp.c
+++ keyboard/satan/keymaps/keymap_mp.c
@@ -9,7 +9,6 @@
 // entirely and just use numbers.
 #define _BM 0
 #define _FL 1
-#define _MV 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -29,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BM] = KEYMAP(
   F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,  KC_EQL,   KC_BSPC, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,  KC_RBRC,  KC_BSLS, \
-  F(3),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,            KC_ENT,  \
+  KC_LCTL,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,          KC_ENT,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,            KC_RSFT, \
-  KC_LCTL, KC_LALT,KC_LGUI,          KC_SPC,                                       KC_RGUI,KC_RALT,  KC_RCTL,  F(1)),
+  F(1), KC_LALT,KC_LGUI,          KC_SPC,                                       KC_RGUI,KC_RALT,  KC_RCTL,  F(1)),
 
   /* Keymap _FL: Function Layer
    * ,-----------------------------------------------------------.
@@ -51,27 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS,KC_TRNS,KC_TRNS,  KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,KC_PGUP,KC_TRNS, KC_PAUS,   KC_MUTE,   KC_VOLD,   KC_VOLU,   KC_TRNS, \
   KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_HOME,KC_LEFT,KC_DOWN,KC_UP,   KC_RGHT,   KC_END,    KC_TRNS,              KC_TRNS, \
   KC_TRNS,        BL_DEC, BL_TOGG,BL_INC, M(0),   KC_TRNS,KC_PGDN,KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS, \
-  KC_TRNS,KC_TRNS,KC_TRNS,          KC_TRNS,                               KC_TRNS,    KC_TRNS,   KC_TRNS,              KC_TRNS),
-
-  /* Keymap 3: Fn1 Layer
-   * ,-----------------------------------------------------------.
-   * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |
-   * |-----------------------------------------------------------|
-   * |     |   |Up |   |   |   |   |   |   |   |   |BL-|BL+|BL   |
-   * |-----------------------------------------------------------|
-   * |      |Lef|Dow|Rig|   |Hom|Lef|Dow|Up |Rig|End|   |        |
-   * |-----------------------------------------------------------|
-   * |        | F1|F2 | F3|F4 | F5| F6| F7| F8|   |   |Up        |
-   * |-----------------------------------------------------------|
-   * |    |    |    |         FN9            |_BW |Left|Down|Rig |
-   * `-----------------------------------------------------------'
-   */
-[_MV] = KEYMAP(
-  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, \
-  KC_NO,  KC_NO,  KC_UP,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  BL_DEC, BL_INC, BL_TOGG, \
-  KC_NO,  KC_LEFT,KC_DOWN,KC_RGHT,KC_NO,  KC_HOME,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_END, KC_NO,          KC_NO, \
-  KC_NO,          KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,          KC_UP, \
-  KC_NO,  KC_NO,  KC_NO,         KC_TRNS,                                         F(2),  KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_TRNS,KC_TRNS,KC_TRNS,          KC_TRNS,                               KC_TRNS,    KC_TRNS,   KC_TRNS,              KC_TRNS)
 };
 
 enum function_id {
@@ -80,9 +59,7 @@ enum function_id {
 
 const uint16_t PROGMEM fn_actions[] = {
   [0] = ACTION_FUNCTION(SHIFT_ESC),
-  [1] = ACTION_LAYER_ON(_MV, ON_PRESS),
-  [2] = ACTION_LAYER_OFF(_MV, ON_PRESS),
-  [3] = ACTION_LAYER_TAP_KEY(_FL, KC_CAPS),
+  [1] = ACTION_LAYER_MOMENTARY(_FL)
 };
 
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
